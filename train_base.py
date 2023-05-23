caab93aa385cb13aa31cebdd1221609fdc5cd4b0
Giang Vu Long <longgiang2015bn@gmail.com>
2023-05-23 11:43:21 +0700

    feat: change lr

--- train_base.py
+++ train_base.py
@@ -21,7 +21,7 @@ def train(model, log_file_name="", clamp_value=-1):
     batch_size = 64
     workers = 5
     # num_epoch = 90
-    lr = 1e-5
+    lr = 0.01
     num_epoch = 20
     # lr = 0.01
     weight_decay = 1e-4
